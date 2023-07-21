1bbc00e688ad93991791e9d7575b963dc10329b3
Nerfinitium <buldukemir1@gmail.com>
2023-07-21 23:20:22 +0300

    SQL Injection Vuln fix

--- src/main/java/me/arch/hoi/Hoi.java
+++ src/main/java/me/arch/hoi/Hoi.java
@@ -2,7 +2,6 @@ package me.arch.hoi;
 
 
 import me.arch.hoi.commands.HClaim;
-import me.arch.hoi.commands.feed;
 import me.arch.hoi.commands.HCreate;
 import me.arch.hoi.listeners.ChunkLoad;
 import me.arch.hoi.listeners.PlayerJoin;
--- src/main/java/me/arch/hoi/commands/HSurrender.java
+++
@@ -1,4 +0,0 @@
-package me.arch.hoi.commands;
-
-public class HSurrender {
-}
